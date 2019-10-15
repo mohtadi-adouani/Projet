283b74d32af827b9d4a34887578f6eae26773211
Mohtadi ADOUANI <moadouani@sciapp14.sciences.lan>
2019-10-15 16:53:56 +0200

    TP4 avec Build

---
+++ tp4/src/main/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+	public static void main(String[] args){
+		ZonedDateTime t1 = 
+		Client c1 =  new Client("Jean","Pierre","Mail:Jean-Pierre@gmail.com","");
+		Passager p1 = new Passager("Marie","Pierre");
+	}
+}
---
+++ tp4/src/main/Vol.java
@@ -0,0 +1,39 @@
+import java.time.*;
+import java.util.ArrayList;
+
+public class Vol{
+
+	private String identifiant;
+	private ZonedDateTime depart;
+	private ZonedDateTime arrivee;
+	public ArrayList<Reservation> Reservations = new ArrayList<Reservation>();
+	public Integer passagerMax;
+
+	Vol(ZonedDateTime d,ZonedDateTime a){
+		this.passagerMax = 217; //Par defaut
+		this.depart = d;
+		this.arrivee = a;
+		this.identifiant = Integer.toString((int)Math.random()*7563)+Integer.toString((int)Math.random()*75635);
+	}
+	//Methodes
+	public void addReservation(Reservation r){
+		this.Reservations.add(r);
+	}
+	//Setteurs
+	public void setdepart(ZonedDateTime d){
+		this.depart = d ;
+	}
+
+	public void setarrivee(ZonedDateTime a){
+		this.arrivee = a;
+	}
+
+	//Getteurs
+	public ZonedDateTime getdepart(){
+		return this.depart;
+	}
+
+	public ZonedDateTime getarrivee(){
+		return this.arrivee;
+	}
+}
